0c3ccd5d71dc2742ffd8d14abfe5f67fbe1d06c4
Praise <osunlekepraise@gmail.com>
2025-11-12 05:04:40 -0800

    created a profile model

--- userapi/models.py
+++ userapi/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+GENDER = (
+    ('Male','Male'),
+    ('Female','Female'),
+)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField()
+    gender = models.CharField(max_length=6, choices=GENDER)
+    phone = models.CharField(max_length=12)
+    photo = models.ImageField(upload_to="profile",default="https://randomuser.me/api/portraits/men/83.jpg", null=True, blank=True)
+
+    def __str__(self):
+        return self.fullname
+
+# Django relationship
+'''
+1 to 1 relationship
+1-1 + 1-1 = 1 to 1
+
+1 to many relationship
+1-1 + 1-many = 1 to many
+
+many to many relationship
+1-many + 1-many = many to many
+'''
